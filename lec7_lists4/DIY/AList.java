776e0dbdc9f3b6ce735a3634ebfc1be83b5bf644
jiaweitang22 <97751503+jiaweitang22@users.noreply.github.com>
2023-06-29 18:07:32 +0800

    Update AList.java

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -5,31 +5,51 @@ package lec7_lists4.DIY;
  *  After writing your methods, you can run the AListTest file.
  */
 public class AList {
+    public int[] array;
+    public int lastIndex;
+    public int size;
     /** Creates an empty list. */
     public AList() {
+        array = new int[10];
+        lastIndex = 0;
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if (lastIndex >= array.size()) {
+            int[] new_array = new int[array.size() * 2];
+            for (int i=0; i<array.size(); i++) {
+                new_array[i] = array[0];
+            }
+            array = new_array;
+        }
+        array[lastIndex] = x;
+        lastIndex++;
+        size++;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return array[lastIndex - 1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return array[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
-    }
+        int temp = array[lastIndex - 1];
+        array[lastIndex - 1] = 0;
+        lastIndex--;
+        size--;
+        return temp;
+            }
 } 
